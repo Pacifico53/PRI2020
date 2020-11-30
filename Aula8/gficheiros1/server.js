95ad2f9c60e57ba1a6dff09048d06490da1a848d
Pacifico53 <bernardo2viseu@gmail.com>
2020-11-30 15:49:57 +0000

    tp7 fix e inicio aula 8

---
+++ Aula8/gficheiros1/server.js
@@ -0,0 +1,44 @@
+var express = require('express')
+var bodyParser = require('body-parser')
+var templates = require('./html-template')
+var jsonfile = require('jsonfile')
+var logger = require('morgan')
+
+var multer = require('multer')
+var upload = multer({ dest: 'uploads/' })
+
+var app = express();
+
+//set logger
+app.use(logger('dev'))
+
+app.use(bodyParser.urlencoded({
+    extended: false
+}))
+
+app.use(bodyParser.json())
+
+app.get('/', function (req, res) {
+    var d = new Date().toISOString().substr(0, 16);
+    var files = jsonfile.readFileSync('./dbFiles.json')
+    res.writeHead(200, { 'Content-Type': 'text/html;charset=utf-8' })
+    res.write(templates.fileList(files, d))
+    res.end()
+})
+
+app.get('/files/upload', function (req, res) {
+    var d = new Date().toISOString().substr(0, 16);
+    res.writeHead(200, { 'Content-Type': 'text/html;charset=utf-8' })
+    res.write(templates.fileForm(d))
+    res.end()
+})
+
+app.post('/files', upload.single('myFile'), function (req, res) {
+    //req.file is the 'myFile'
+    //req.body will not hold the text fields if any
+    res.writeHead(200, { 'Content-Type': 'text/html;charset=utf-8' })
+    res.write('<pre>' + JSON.stringify(req.file) + '</pre>')
+    res.end()
+})
+
+app.listen(7701, () => console.log('Listening on 7701'));
---
+++ Aula8/middleware/server.js
@@ -0,0 +1,25 @@
+var express = require('express')
+var bodyParser = require('body-parser')
+
+var app = express()
+
+app.use(bodyParser.urlencoded({
+    extended: false
+}))
+
+app.use(bodyParser.json())
+
+app.use(function (req, res, next) {
+    console.log(JSON.stringify(req.body));
+    next();
+})
+
+app.get('*', function (req, res) {
+    res.status(200).send('Hello World');
+})
+
+app.post('*', function (req, res) {
+    res.status(201).json(JSON.stringify(req.body))
+})
+
+app.listen(7700, () => console.log('Servidor está a escuta na porta 7700'))
