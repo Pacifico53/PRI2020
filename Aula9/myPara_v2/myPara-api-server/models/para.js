5d99555e05fb8579d7c82ab302cbcb1ca0bae310
Pacifico53 <bernardo2viseu@gmail.com>
2020-12-07 17:08:18 +0000

    aula9

---
+++ Aula9/myPara_v2/myPara-api-server/models/para.js
@@ -0,0 +1,9 @@
+// Paragraph model-----------------------
+var mongoose = require('mongoose')
+
+var paraSchema = new mongoose.Schema({
+    text: String
+})
+
+module.exports = mongoose.model('para', paraSchema)
+
